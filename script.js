11b5e2f35c9724b124161dc27ba30f5330eacde6
austin-jordan <austin.j4106@gmail.com>
2016-03-07 16:56:40 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+  $('#logo').hover(function() {
+    $(this).css("color", "#C0C0C0");
+  },
+  function() {
+    $(this).css("color", "#D8D8D8");
+  });
+
+  $('#logo').hover(function() {
+    $(this).css("border-color", "#C0C0C0");
+  },
+  function() {
+    $(this).css("border-color", "#D8D8D8");
+  });
+
+  $('nav a').hover(function() {
+    $(this).css("color", "#606060");
+  },
+  function() {
+    $(this).css("color", "#D8D8D8");
+  });
+
+  $('select').hover(function() {
+    $(this).css("background-color", "#A0A0A0");
+  },
+  function() {
+    $(this).css("background-color", "#f8f7f7");
+  });
+
+  // $('select').click(function() {
+  // $('select').selectmenu({
+  //   background: "#A0A0A0"
+  // });
+
+  $(':submit').hover(function() {
+    $(this).css("background-color", "#A0A0A0");
+  },
+  function() {
+    $(this).css("background-color", "#f8f7f7");
+  });
+
+  $(':checkbox').hover(function() {
+    $(this).css("background-color", "#A0A0A0");
+  },
+  function() {
+    $(this).css("background-color", "#f8f7f7");
+  });
+
+});
